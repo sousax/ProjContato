5215f9087495603c2f73efe7121a45b10100261e
Eduardo Miranda <102879023+sousax@users.noreply.github.com>
2024-11-04 15:22:28 -0300

    Add files via upload

---
+++ ProjContato/Telefone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjContato
+{
+    internal class Telefone
+    {
+        private string tipo;
+        private string numero;
+        private bool principal;
+
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string Numero { get => numero; set => numero = value; }
+        public bool Principal { get => principal; set => principal = value; }
+    }
+}
+
+//Eduardo Miranda CB3026604
